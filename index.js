d9971dbf37cfca5007cb230bb2e8f99f4af3ee2e
zoff4ick <sviatoslav.h@creabl.com>
2023-10-30 20:29:07 +0200

    Added some new functions

--- index.js
+++ index.js
@@ -41,4 +41,102 @@ const promisify = (f) => {
   }
 }
 
-module.exports = {asyncify, promisify};
+const promisifySync = (f) => {
+  if (typeof f !== 'function') throw new Error('f must be a function');
+
+  return (...args) => {
+    try {
+      const result = f(...args);
+      if (result instanceof Error) return Promise.reject(result);
+
+      return Promise.resolve(result);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+}
+
+const callbackify = (f) => {
+  if (typeof f !== 'function') throw new Error('f must be a function');
+
+  return (...args) => {
+    const wrappyArgsNumberWithCallback = f.length + 1;
+    if (args.length !== wrappyArgsNumberWithCallback) {
+      throw new Error('Wrong number of arguments');
+    }
+
+    const callback = args.pop();
+    if (typeof callback !== 'function') {
+      throw new Error('Callback is required!');
+    }
+
+    f(...args)
+      .then(res => callback(null, res))
+      .catch(err => callback(err));
+  }
+}
+
+const expirify = (f, timeout) => {
+  if (!f || !timeout) {
+    throw new Error('Both arguments are required!');
+  }
+
+  if (typeof f !== 'function') {
+    throw new Error('f must be a function');
+  }
+
+  if (typeof timeout !== 'number') {
+    throw new Error('timeout must be type number');
+  }
+
+  let targetFunction = f;
+
+  setTimeout(() => targetFunction = null, timeout);
+
+  return (...args) => {
+    if (typeof targetFunction === 'function') {
+      return targetFunction(...args);
+    }
+  }
+}
+
+const cancelable = (f) => {
+  if (typeof f !== 'function') {
+    throw new Error('f must be a function');
+  }
+
+  let targetFunction = f;
+  let canceled = null;
+
+  const functionToReturn = (...args) => {
+    if (typeof targetFunction === 'function') {
+      return targetFunction(...args);
+    }
+  };
+
+  functionToReturn.cancel = () => {
+    targetFunction = null;
+    canceled = true;
+  }
+
+  return functionToReturn;
+}
+
+const once = (f) => {
+  if (typeof f !== 'function') {
+    throw new Error('f must be a function');
+  }
+
+  let targetFunction = f;
+
+  return (...args) => {
+    if (typeof targetFunction === 'function') {
+      const targetFunctionCopy = targetFunction
+      targetFunction = null;
+
+      return targetFunctionCopy(...args);
+    }
+  };
+}
+
+module.exports = {asyncify, promisify, promisifySync, callbackify, expirify, cancelable, once};
